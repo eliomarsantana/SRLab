d8541dcb98b7d042c4c0363c9adb60e6c5c83af0
felipepcoelho <felipe.pereira.coelho@gmail.com>
2015-03-28 21:47:34 -0300

    'Inserção de URLs e index'

--- SRLab/urls.py
+++ SRLab/urls.py
@@ -5,6 +5,8 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'SRLab.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^Administrativo/', include('Administrativo.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+
